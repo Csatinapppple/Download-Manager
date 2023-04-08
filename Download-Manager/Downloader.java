3faa6230714e5cf2d1b60bb44f84712ad78466df
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-04-08 00:41:49 -0300

    Add files via upload

--- Download-Manager/Downloader.java
+++ Download-Manager/Downloader.java
@@ -5,10 +5,11 @@ public class Downloader {
   
   String path,url_source;
   Date time_started_downloading;
-
+  boolean successful;
   public Downloader(String path,String url_source){
     this.path=path;this.url_source=url_source;
     time_started_downloading = new Date();
+    successful=false;
   }
 
   void tryToDownload(){
--- Download-Manager/Main.java
+++ Download-Manager/Main.java
@@ -1,18 +1,42 @@
 import java.io.InputStream;
+import java.lang.StringBuilder;
 import java.net.URL;
 import java.nio.file.*;
 
 class Main
 {
-    public static void downloadFile(URL url, String filename) throws Exception
-    {
-        try (InputStream in = url.openStream()){
-          Files.copy(in, Paths.get(filename));
-        }
+
+  public static void downloadFile(URL url, String filename) throws Exception
+  {
+    try (InputStream in = url.openStream()){
+      Files.copy(in, Paths.get(filename));
     }
+  }
+
+  static String getFilenameReverse(String url){
+    String ret = "";
+    for(int i = url.length()-1; i>=0;i--){
+      if(url.charAt(i)!='/')
+        ret+=url.charAt(i);
+      else
+        break;
+    }
+    return ret;
+  }
+
   //nexuiz in brazil https://razaoinfo.dl.sourceforge.net/project/nexuiz/NexuizRelease/Nexuiz%202.5.2/nexuiz-252.zip
   public static void main(String[]args) throws Exception{
-    URL url = new URL("https://razaoinfo.dl.sourceforge.net/project/nexuiz/NexuizRelease/Nexuiz%202.5.2/nexuiz-252.zip");
-    downloadFile(url, "nexuiz.zip");
+    if(args.length != 1){
+      printUsage();
+      return;
+    }
+    URL url = new URL(args[0]);
+    StringBuilder filename = new StringBuilder(
+      getFilenameReverse(args[0])).reverse();
+    downloadFile(url, filename.toString());
+  }
+
+  static void printUsage(){
+    System.out.printf("Usage: program url\n");
   }
 }
