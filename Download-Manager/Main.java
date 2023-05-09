bca0a89e80b352f0e2661c493f3ade7234853f55
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-05-08 23:32:43 -0300

    Add files via upload
    cli version = better

--- Download-Manager/Main.java
+++ Download-Manager/Main.java
@@ -5,7 +5,7 @@ import java.nio.file.*;
 import java.util.Scanner;
 
 class Main {
-  String defaultDirectory = "";
+  static String default_directory = System.getProperty("user.home"+"/Downloads");
 
   // nexuiz in brazil
   // https://razaoinfo.dl.sourceforge.net/project/nexuiz/NexuizRelease/Nexuiz%202.5.2/nexuiz-252.zip
@@ -16,41 +16,20 @@ class Main {
 
     if (args.length == 1)
       staticDownload(args[0]);
-
-    int input = 0;
-    Scanner s = new Scanner(System.in);
-    while (input != 3) {
-      printOptions();
-      input = s.nextInt();
-      handleChoices(input);
-    }
+		else
+			printUsage();
   }
 
-  static void handleChoices(int choice) throws IOException {
-    Scanner s = new Scanner(System.in);
-    switch (choice) {
-      case 1:
-        Downloader download = new Downloader("", s.next());
-        download.downloadFile();
-        FileTools.appendFile(download.toString(),"history.csv");
-        break;
-      case 2:
-        break;
-      default:
-        break;
-    }
-  }
+	static final void printUsage();{
+		System.out.println("usage: java Main -DOWNLOADURL");
+	}
+
   //0  +2 +3 -1 +11 -8 +23 -19
   //13,15,18,17, 28,20, 43, 24,x
 
   /*
     7 6 5 4 3 2 1
    */
-  static void printOptions() {
-    System.out.println("1. Download from url");
-    System.out.println("2. Print download history");
-    System.out.println("3. Exit");
-  }
 
   static void staticDownload(String url) throws IOException {
     Downloader d = new Downloader("", url);
